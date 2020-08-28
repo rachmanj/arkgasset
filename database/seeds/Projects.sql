0dc8737449a6b53e050ce940c5ba2c87befd2f1d
rachmanj <rachmanj@gmail.com>
2020-08-28 14:41:37 +0800

    projects

---
+++ database/seeds/Projects.sql
@@ -0,0 +1,13 @@
+INSERT INTO projects (id, code, location, bowheer, active) VALUES
+(1, '000H', 'Balikpapan', 'Head Office Balikpapan', 'Y'),
+(2, '001H', 'Jakarta', 'Branch Office Jakarta', 'Y'),
+(3, '011C', 'Embalut', 'PT. Kitadin', 'Y'),
+(4, '015C', 'Penajam', 'PT. Penajam Prima Coal', 'N'),
+(5, '016C', 'Sanga-sanga', 'PT. Adimitra Baratama Nusantara', 'N'),
+(7, '017C', 'Malinau', 'PT. Kayan Putra Utama Coal', 'Y'),
+(8, '018C', 'Tanah Grogot', 'CV. Bukit Uhud', 'N'),
+(9, '004W', 'Kariangau', 'PT. Adikara Mulia', 'Y'),
+(11, '008C', 'Senoni', 'PT. Tanito', 'N'),
+(12, '005P', 'Maratua', 'PT. Pratasaba', 'Y'),
+(13, 'APS', 'Kariangau', 'Arka Project Support', 'Y'),
+(14, '019C', 'Sesayap', 'PT. Sumber Mandiri Utama', 'N');
---
+++ database/seeds/ProjectsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $sql = file_get_contents(database_path() . '/seeds/projects.sql');
+
+        DB::statement($sql);
+    }
+}
